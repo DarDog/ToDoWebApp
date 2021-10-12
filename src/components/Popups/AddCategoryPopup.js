af964096831004c3f96428e50357027d5bcc862e
DarDog <samediacc666@gmial.com>
2021-10-12 23:27:48 +1100

    Feat: make form validation for AddCategoryPopup form

--- src/components/Popups/AddCategoryPopup.js
+++ src/components/Popups/AddCategoryPopup.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Popup from "./Popup";
+import FormErrors from "./FormErrors";
 
 function AddCategoryPopup(props) {
   const [name, setName] = React.useState('');
+  const [isNameValid, setIsNameValid] = React.useState(false);
+  const [nameErrorMassage, setNameErrorMassage] = React.useState('');
+  const [isFormValid, setIsFormValid] = React.useState(false);
 
   const handleNameChange = (e) => {
     setName(e.target.value)
+    handleNameValid(e.target)
   }
+
+  const handleNameValid = (input) => {
+    if (!input.validity.valid) {
+      setIsNameValid(false)
+      setNameErrorMassage(input.validationMessage)
+    } else {
+      setIsNameValid(true)
+      setNameErrorMassage('')
+    }
+  }
+
+  React.useEffect(() => {
+    if (!isNameValid) {
+      setIsFormValid(false)
+    } else {
+      setIsFormValid(true)
+    }
+  }, [isNameValid, props.isOpen])
 
   return (
       <Popup
@@ -15,18 +38,20 @@ function AddCategoryPopup(props) {
           popupName={'Добавить новую категорию'}
           formName={'category-creator'}
           isAddTask={false}
+          isValid={isFormValid}
       >
         <label className="form__label" htmlFor="input-task-name">Что нужно сделать?</label>
-        <input className={`form__input`}
+        <input className={`form__input ${isNameValid ? 'form__input_valid' : '' }`}
                type="text"
                id="input-task-name"
                name="taskName"
                required
-               maxLength="100"
+               maxLength="30"
                minLength="2"
                onChange={handleNameChange}
                value={name || ''}
         />
+        <FormErrors isValid={isNameValid} errorsMassage={nameErrorMassage} />
       </Popup>
   );
 }
