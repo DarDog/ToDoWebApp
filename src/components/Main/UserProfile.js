f373756c9f8950cc675952717fbe8773b903a82b
DarDog <samediacc666@gmial.com>
2021-10-24 15:56:55 +1100

    feat: UserProfile make validation

--- src/components/Main/UserProfile.js
+++ src/components/Main/UserProfile.js
@@ -1,7 +1,45 @@
 import React from 'react';
 import avatar from '../../images/avatar.jpg'
+import FormErrors from "../Popups/FormErrors";
 
 function UserProfile(props) {
+  const [name, setName] = React.useState(''),
+      [isNameValid, setIsNameValid] = React.useState(false),
+      [nameErrorMassage, setNameErrorMassage] = React.useState(''),
+      [email, setEmail] = React.useState(''),
+      [isEmailValid, setIsEmailValid] = React.useState(false),
+      [emailErrorMassage, setEmailErrorMassage] = React.useState('')
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+    handleNameValidity(e.target)
+  }
+
+  const handleNameValidity = (input) => {
+    if (!input.validity.valid) {
+      setIsNameValid(false);
+      setNameErrorMassage(input.validationMessage);
+    } else {
+      setIsNameValid(true);
+      setNameErrorMassage('');
+    }
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+    handleEmailValidity(e.target)
+  }
+
+  const handleEmailValidity = (input) => {
+    if (!input.validity.valid) {
+      setIsEmailValid(false);
+      setEmailErrorMassage(input.validationMessage);
+    } else {
+      setIsEmailValid(true)
+      setEmailErrorMassage('')
+    }
+  }
+
   return (
       <article className={`container ${props.isThemeDark && 'container_theme_dark'} main__container`}>
         <div className="user-profile">
@@ -12,12 +50,27 @@ function UserProfile(props) {
           <div className="user-profile__menu">
             <form className="form user-profile__form">
               <div className='user-profile__form-container'>
-                <label htmlFor="" className="form__label">Ваш никнейм:</label>
-                <input type="text" className="form__input"/>
+                <label htmlFor="" className={`form__label ${props.isThemeDark && 'form__label_theme_dark'}`}>Ваш никнейм:</label>
+                <input
+                    type="text"
+                    className="form__input"
+                    minLength={3}
+                    required
+                    value={name}
+                    onChange={handleNameChange}
+                />
+                {nameErrorMassage.length > 0 && <FormErrors isValid={isNameValid} errorsMassage={nameErrorMassage} />}
               </div>
               <div className='user-profile__form-container'>
-                <label htmlFor="" className="form__label">Ваша почта:</label>
-                <input type="text" className="form__input"/>
+                <label htmlFor="" className={`form__label ${props.isThemeDark && 'form__label_theme_dark'}`}>Ваша почта:</label>
+                <input
+                    type="email"
+                    className="form__input"
+                    required
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+                {emailErrorMassage.length > 0 && <FormErrors isValid={isEmailValid} errorsMassage={emailErrorMassage} />}
               </div>
               <div className="form__controller form__controller_arrange_closer">
                 <button className="button form__button form__button_type_submit">Сохранить изменения</button>
